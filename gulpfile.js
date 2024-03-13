565c089a261555ed254b8395517202c47ca70990
jecheverria0 <jecheverria_@outlook.com>
2024-03-12 23:56:08 -0600

    -Se completo el diseño del video, se le agrego el titulo y el lugar. -Se creo un archivo _festival para añadir la descripcion del festical, tambien se agrego un mixin para el deseño del grid -Se instalaron dependencias papara convertir imagenes -Se crearon dos fuciones dentro del archivo gulpfile para convertir imagenes JPG y PNG a formato Avif y Webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+//IMAGENES 
+const avif = require('gulp-avif');
+
 
 function css (done) {
     src("src/scss/**/*.scss") //Identificar el archivo SASS
@@ -11,6 +17,32 @@ function css (done) {
     done(); //Callback, avisa a gulp cuando terminamos de ejecutar la funcion
 }
 
+async function versionWebp (done){
+    const webp = await  import('gulp-webp');
+    const opciones = {
+        quality : 50   
+    }
+
+    src('src/img/**/*.{jpg,JPG,png,PNG}')
+        .pipe(webp.default(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function versionAvif( done ){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{jpg,JPG,png,PNG}')
+        .pipe ( avif(opciones) )
+        .pipe ( dest('build/img'))
+
+    done();
+}
+
 function dev (done) {
     watch("src/scss/**/*.scss", css)
 
@@ -18,4 +50,6 @@ function dev (done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel (versionWebp, versionAvif, dev);
