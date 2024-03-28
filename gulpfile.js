b326f58796ec6ffa4ca1c2f56c1ed3c3c52f683f
jecheverria0 <jecheverria_@outlook.com>
2024-03-28 10:31:28 -0600

    Se agrego una nueva funcion en gulpfile, se creo un nuevo archivo en layout para la galeria de la pagina y se agrego codigo javascript

--- gulpfile.js
+++ gulpfile.js
@@ -43,13 +43,23 @@ function versionAvif( done ){
     done();
 }
 
+function javascript(done){
+
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev (done) {
-    watch("src/scss/**/*.scss", css)
+    watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
 
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel (versionWebp, versionAvif, dev);
+exports.dev = parallel (versionWebp, versionAvif, javascript, dev);
